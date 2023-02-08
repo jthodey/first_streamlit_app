ea5fbfcff1643ebfac518426a1d57d477841b08b
jthodey <123339169+jthodey@users.noreply.github.com>
2023-02-08 13:57:34 +1100

    Update streamtlit_app.py

--- streamtlit_app.py
+++ streamtlit_app.py
@@ -38,12 +38,12 @@ streamlit.dataframe(fruityvice_normalized)
 ## establish the connection to snowflake. Note that the requirements doc is used to say we use python
 import snowflake.connector
 
-## test the connection
+## query data from snowflake in our streamlit app
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
 
